705a3ffc8720c7115455decb3fd0923617ca6052
gabriele <gc787618@ohio.edu>
2020-10-30 15:51:31 -0400

    new files

--- course.cc
+++ course.cc
@@ -15,7 +15,7 @@ course::course(){
 }
 
 void course::input(std::istream& ins){
-    if(ins == cin){
+    if(&ins == &cin){
 	cout<<"Course Number: ";
 	if(ins.peek() == '\n') ins.ignore();
     	getline(ins, course_number);
@@ -40,7 +40,7 @@ void course::input(std::istream& ins){
 }
 
 void course::output(std::ostream& outs)const{
-    if(outs == cout){
+    if(&outs == &cout){
 	outs<<"Course Number:"<<course_number<<endl;
 	outs<<"Grade received:"<<grade<<endl;
 	outs<<"Credit hours:"<<setprecision(2)<<hours<<endl;
